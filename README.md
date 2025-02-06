085c9ee05dd433bb4d850d50b18748cbbfa7e860
FreyGeospatial <jfrey0725@gmail.com>
2025-02-05 21:24:25 -0500

    updated flask app and docs

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ A Flask application is an instance of the Flask class. Everything about the appl
 The most straightforward way to create a Flask app is to create a global Flask instance directly at the top of your code. This is a simple approach, and while useful, it can cause issues as projects grow.
 
 It is instead better to create a Flask instance inside a function, which is known as the application factory. All configuration, registration and setup the app needs will happen there.
+
+## how to run app/api:
+`flask --app flaskr run --debug`
+
+If you load http://127.0.0.1:5000/hello in a web browser, you will get the text or html returned by the application, as well as a the print statement in the console.
+
+If you run `curl http://127.0.0.1:5000/hello`, anything that is returned by the app will be returned by the API.
---
+++ flaskr/__init__.py
@@ -0,0 +1,55 @@
+"""
+This module will contain the application factory, 
+and it tells Python that the flaskr directory should be 
+treated as a package.
+"""
+
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """
+    Appfactory function
+    """
+
+    # create and configure the app.
+
+    # __name__ is the name of the current Python module.
+    # The app needs to know where it’s located to set up some paths,
+    # and __name__ is a convenient way to tell it that.
+
+    # instance_relative_config=True tells the app that configuration
+    # files are relative to the instance folder.
+    # The instance folder is located outside the flaskr package and
+    # can hold local data that shouldn’t be committed to version control,
+    # such as configuration secrets and the database file.
+    app = Flask(__name__, instance_relative_config=True)
+
+    # app.config.from_mapping() sets some default configuration that the app will use
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route("/hello")
+    def hello():
+        print("hello")
+        return "Hello, World!"
+
+    return app
